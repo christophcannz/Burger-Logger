c110dfa7163f1ee8b699c16f766f118b1a2386ec
christophcannz <christophcannz@gmail.com>
2019-06-03 15:15:10 -0500

    mon2

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -13,7 +13,6 @@ router.get("/", function (req, res) {
         console.log('burgers: ', burgers);
         res.render("index", { burgers});
     });
-
 });
 
 module.exports = router;
--- db/config/connection.js
+++ db/config/connection.js
@@ -3,6 +3,7 @@ const mysql = require("mysql");
 
 let connection = mysql.createConnection({
     host: "localhost",
+    port: "3306",
     user: "root",
     password: "root",
     database: "burger_logger_db"
